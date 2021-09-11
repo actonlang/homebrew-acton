a1ae9d7add2870093df263887c88f51138a0546a
Kristian Larsson <kristian@spritelink.net>
2021-09-12 00:10:23 +0200

    acton 0.5.3 (new formula)
    Closes #3.
    
    Signed-off-by: Kristian Larsson <841628+plajjan@users.noreply.github.com>

---
+++ Formula/acton.rb
@@ -0,0 +1,40 @@
+class Acton < Formula
+  desc "Awesome Programming Language"
+  homepage "http://www.acton-lang.org"
+  url "https://github.com/actonlang/acton/archive/refs/tags/v0.5.3.tar.gz"
+  sha256 "888cc6a8ecca5eeb3e144f64006ad9ea52a358312576c632c73280c483ffa8b6"
+  license "BSD-3-Clause"
+  head "https://github.com/actonlang/acton.git", branch: "main"
+
+  depends_on "ghc" => :build
+  depends_on "haskell-stack" => :build
+  depends_on "utf8proc" => :build
+
+  depends_on "protobuf-c"
+  depends_on "util-linux"
+
+  on_macos do
+    depends_on "argp-standalone" => :build
+  end
+
+  def install
+    ENV.deparallelize
+    system "make"
+    bin.install "dist/bin/actonc"
+    bin.install "dist/bin/actondb"
+    prefix.install Dir["dist/*"]
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test acton`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "true"
+  end
+end
