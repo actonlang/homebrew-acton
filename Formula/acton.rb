03535729afb1f36c943035cf969ca4812376793e
Kristian Larsson <kristian@spritelink.net>
2021-09-13 13:57:29 +0200

    acton v0.5.3 fix test comment

--- Formula/acton.rb
+++ Formula/acton.rb
@@ -31,12 +31,10 @@ class Acton < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test acton`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    # For Homebrew/homebrew-core this will need to be a test that verifies the
+    # functionality of the software. Run the test with `brew test acton`.
+    # Options passed to `brew install` such as `--HEAD` also need to be
+    # provided to `brew test`.
     #
     # The installed folder is not in the path, so use the entire path to any
     # executables being tested: `system "#{bin}/program", "do", "something"`.
