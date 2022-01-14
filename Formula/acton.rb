f2033887afed09cbf68d7207f9fd8f276c7f1123
plajjan <plajjan@users.noreply.github.com>
2022-01-14 10:02:55 +0000

    acton v0.8.0

--- Formula/acton.rb
+++ Formula/acton.rb
@@ -1,8 +1,8 @@
 class Acton < Formula
   desc "Safe actor-based programming language"
   homepage "https://www.acton-lang.org"
-  url "https://github.com/actonlang/acton/archive/refs/tags/v0.7.1.tar.gz"
-  sha256 "e58487e8c5b17a669080714a2b9940e02f2e26c86e7a549b0fdcbde92ea866ae"
+  url "https://github.com/actonlang/acton/archive/refs/tags/v0.8.0.tar.gz"
+  sha256 "0d35eeb4fac6fce27e6d55a9ddbb2448a39acbaf252554cdcf1e5ca27e7acaaf"
   license "BSD-3-Clause"
   head "https://github.com/actonlang/acton.git", branch: "main"
 
