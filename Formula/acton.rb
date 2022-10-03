a8917d7ddcdee33498f8aef62f7ea5ec32f89672
plajjan <plajjan@users.noreply.github.com>
2022-10-03 17:02:24 +0000

    acton v0.11.7

--- Formula/acton.rb
+++ Formula/acton.rb
@@ -1,17 +1,10 @@
 class Acton < Formula
   desc "Delightful distributed programming language"
   homepage "https://www.acton-lang.org"
-  url "https://github.com/actonlang/acton/archive/refs/tags/v0.11.6.tar.gz"
-  sha256 "028cd83698d136377c27a73e116e52c35c486943efca733fc7e92e85ec199c57"
+  url "https://github.com/actonlang/acton/archive/refs/tags/v0.11.7.tar.gz"
+  sha256 "980db27886eb441b885e884a757fb9d1e772a227c1490bf95fe0625951075520"
   license "BSD-3-Clause"
   head "https://github.com/actonlang/acton.git", branch: "main"
-
-  bottle do
-    root_url "https://github.com/actonlang/homebrew-acton/releases/download/acton-0.11.6"
-    sha256 cellar: :any_skip_relocation, monterey:     "40544b1254ade3ff2e1bbba0c5f1e742a18c2a4e1a06f0391753fde2e782b3af"
-    sha256 cellar: :any_skip_relocation, big_sur:      "740faaedb4cc683c9849c9ae1ec011c60a60c058d07742c07f9a09a5700f5192"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "20f26d9178916670d9929401f96f0bc978abd89bc5177056334e3ab2f8eb9709"
-  end
 
   depends_on "ghc@8.10" => :build
   depends_on "haskell-stack" => :build
@@ -45,6 +38,7 @@ class Acton < Formula
     bin.install "dist/bin/actondb"
     bin.install "dist/bin/runacton"
     prefix.install Dir["dist/*"]
+    bash_completion.install "completion/acton.bash-completion"
   end
 
   test do
