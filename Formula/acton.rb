61e7359ffe8b7c368c3d1b5aa7c319d43365b682
plajjan <plajjan@users.noreply.github.com>
2023-06-16 20:36:38 +0000

    acton v0.15.2
    Closes #52.
    
    Signed-off-by: Kristian Larsson <841628+plajjan@users.noreply.github.com>

--- Formula/acton.rb
+++ Formula/acton.rb
@@ -1,30 +1,19 @@
 class Acton < Formula
   desc "Delightful distributed programming language"
   homepage "https://www.acton-lang.org"
-  url "https://github.com/actonlang/acton/archive/refs/tags/v0.15.1.tar.gz"
-  sha256 "a85c895f6330fae7e31b28e9308007ffe40ec614c62e4efeef8113e1e64d496e"
+  url "https://github.com/actonlang/acton/archive/refs/tags/v0.15.2.tar.gz"
+  sha256 "cc885025e9eeb20aad86bf8e28e9161a62934999feeb6bd8876488a77751269f"
   license "BSD-3-Clause"
   head "https://github.com/actonlang/acton.git", branch: "main"
 
-  bottle do
-    root_url "https://github.com/actonlang/homebrew-acton/releases/download/acton-0.15.1"
-    sha256 cellar: :any_skip_relocation, ventura: "75e66ffc41adf39a2ace184d2b66ffabbec04e22db65f0826d63ddee87347b2c"
-  end
-
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
+  # TODO: can gettext be removed? it was likely necessary when we built deps
+  # using autoconf, but might not be needed now that we use build.zig?
   depends_on "gettext" => :build
   depends_on "ghc@8.10" => :build
   depends_on "haskell-stack" => :build
-  depends_on "libtool" => :build
-  depends_on "pkg-config" => :build
-  depends_on "protobuf-c" => :build
 
   on_linux do
-    depends_on "libbsd" => :build
-    depends_on "gcc"
     depends_on "gmp"
-    depends_on "util-linux"
   end
 
   def install
