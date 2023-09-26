0ef8df2da3dc47b09b40a9998bbd49c61760644b
plajjan <plajjan@users.noreply.github.com>
2023-09-26 20:18:20 +0000

    acton v0.17.0
    Closes #55.
    
    Signed-off-by: Kristian Larsson <841628+plajjan@users.noreply.github.com>

--- Formula/acton.rb
+++ Formula/acton.rb
@@ -1,17 +1,10 @@
 class Acton < Formula
   desc "Delightful distributed programming language"
   homepage "https://www.acton-lang.org"
-  url "https://github.com/actonlang/acton/archive/refs/tags/v0.15.3.tar.gz"
-  sha256 "5b0281074504caaba2ac3d95d507ffdb1fad9d0ce27196fce375dfcf6205eeff"
+  url "https://github.com/actonlang/acton/archive/refs/tags/v0.17.0.tar.gz"
+  sha256 "02e9968b1b36cae4d8dce938f39cecb60866b8d89812336852eab566848f3ee1"
   license "BSD-3-Clause"
   head "https://github.com/actonlang/acton.git", branch: "main"
-
-  bottle do
-    root_url "https://github.com/actonlang/homebrew-acton/releases/download/acton-0.15.3"
-    sha256 cellar: :any_skip_relocation, ventura:      "abc9632f2eced75edc9ce96e7ebbe68e6dd46cb8a63ce1491607cd7184839082"
-    sha256 cellar: :any_skip_relocation, monterey:     "19231ae0f3c663d16cb5e0f7c3f2888b3344c881cf645b824f3fbd7f2a59264f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "c61a62b7ab081a208f3ffe39a2cfb09fae2fb4e967feef1569019fef13f87a9c"
-  end
 
   # TODO: can gettext be removed? it was likely necessary when we built deps
   # using autoconf, but might not be needed now that we use build.zig?
