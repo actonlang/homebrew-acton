2f69b0a3ea250f6639feb830f3a0da013f84b7b8
Kristian Larsson <841628+plajjan@users.noreply.github.com>
2023-10-31 12:38:34 +0000

    acton: add 0.18.1 bottle.

--- Formula/acton.rb
+++ Formula/acton.rb
@@ -6,6 +6,13 @@ class Acton < Formula
   license "BSD-3-Clause"
   head "https://github.com/actonlang/acton.git", branch: "main"
 
+  bottle do
+    root_url "https://github.com/actonlang/homebrew-acton/releases/download/acton-0.18.1"
+    sha256 cellar: :any_skip_relocation, ventura:      "ff1b8c9b713ef60f9190576cd971480cd52f0ae4cda652554cb9266e1af65d6e"
+    sha256 cellar: :any_skip_relocation, monterey:     "efbcac7c3b4b05c4e11f85b9959785ccb824e31b4cf02a4e82f16379b3ec1e02"
+    sha256                               x86_64_linux: "c5576b2cb6c86ce54d2d0c895c9cccd34bbf7ac31e13a35a4d97f04e7d3fd59d"
+  end
+
   depends_on "ghc@9.4" => :build
   depends_on "haskell-stack" => :build
 
