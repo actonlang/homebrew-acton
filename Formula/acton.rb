d32dfc9eff4be02dd45011d8260743013f8d43f5
plajjan <plajjan@users.noreply.github.com>
2023-10-31 14:03:18 +0000

    acton v0.18.2
    Closes #58.
    
    Signed-off-by: Kristian Larsson <841628+plajjan@users.noreply.github.com>

--- Formula/acton.rb
+++ Formula/acton.rb
@@ -1,17 +1,10 @@
 class Acton < Formula
   desc "Delightful distributed programming language"
   homepage "https://www.acton-lang.org"
-  url "https://github.com/actonlang/acton/archive/refs/tags/v0.18.1.tar.gz"
-  sha256 "9c075ef3c3ca50a14e39f8279d45716af57ca044b003b657ed7e1ef040e65905"
+  url "https://github.com/actonlang/acton/archive/refs/tags/v0.18.2.tar.gz"
+  sha256 "9da46bb992f43a17b6f9e455ee7feeed586c8c739fec88065d4c400db9da529d"
   license "BSD-3-Clause"
   head "https://github.com/actonlang/acton.git", branch: "main"
-
-  bottle do
-    root_url "https://github.com/actonlang/homebrew-acton/releases/download/acton-0.18.1"
-    sha256 cellar: :any_skip_relocation, ventura:      "ff1b8c9b713ef60f9190576cd971480cd52f0ae4cda652554cb9266e1af65d6e"
-    sha256 cellar: :any_skip_relocation, monterey:     "efbcac7c3b4b05c4e11f85b9959785ccb824e31b4cf02a4e82f16379b3ec1e02"
-    sha256                               x86_64_linux: "c5576b2cb6c86ce54d2d0c895c9cccd34bbf7ac31e13a35a4d97f04e7d3fd59d"
-  end
 
   depends_on "ghc@9.4" => :build
   depends_on "haskell-stack" => :build
