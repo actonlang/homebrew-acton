9198b1a8830ed5ae4a009b2a4feb2fbf10a36a0d
plajjan <841628+plajjan@users.noreply.github.com>
2026-09-03 14:18:49 +0000

    acton v0.30.0

--- Formula/acton.rb
+++ Formula/acton.rb
@@ -1,15 +1,10 @@
 class Acton < Formula
   desc "Delightful distributed programming language"
   homepage "https://acton.now/"
-  url "https://github.com/actonlang/acton/archive/refs/tags/v0.29.1.tar.gz"
-  sha256 "2dfd750f43ceab39678d26a86789ea6c7e2a85e80bedb400279c89b9417faf5c"
+  url "https://github.com/actonlang/acton/archive/refs/tags/v0.30.0.tar.gz"
+  sha256 "4f0cdc597f20743a5f9a9fe566a0794c8572c00d357f4e02022848aa68589328"
   license "BSD-3-Clause"
   head "https://github.com/actonlang/acton.git", branch: "main"
-
-  bottle do
-    root_url "https://github.com/actonlang/homebrew-acton/releases/download/acton-0.29.1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "f36596dff8595a781ef97dba0cb7717034727d5892cbb26b5a3a901affadecb4"
-  end
 
   depends_on "ghc@9.8" => :build
   depends_on "haskell-stack" => :build
